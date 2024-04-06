ed6d6a265f29c244abcdf9bdc0d6e3885728ea0d
harakazuki <160248478+harakazuki@users.noreply.github.com>
2024-04-06 19:12:27 +0000

    show link

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,19 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
+
+  def index
+    @books = Book.all
+    @book = Book.new  
+  end
+  
+
+  def show
+    @book = Book.find(params[:id])
   end
   
   def create
     @book = Book.new(book_params)
     @book.save
-    redirect_to '/top'
-  end
-
-  def index
-  end
-
-  def show
+    redirect_to '/books'
   end
 
   def edit
